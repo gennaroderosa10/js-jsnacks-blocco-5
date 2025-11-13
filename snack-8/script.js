af231e218d9e03825c542a1f5e0131ea87a03ee1
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-11-13 16:41:43 +0100

    feat: 8 done

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,11 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const marcoLanci = students.find(function (studente) {
+  if (studente.name = "Marco Lanci") {
+    return studente.class;
+  }
+});
+
+console.log(marcoLanci.class);
