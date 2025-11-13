953adaef55edf9f15e677ab745eb8e5417d5a3cf
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-11-13 16:33:12 +0100

    feat: 6 done

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,21 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineGrandi = zucchine.filter(function (zucchina) {
+  if (zucchina.length >= 15) {
+    return true
+  }
+  return false
+});
+
+console.log(zucchineGrandi);
+
+const zucchinePiccole = zucchine.filter(function (zucchina) {
+  if (zucchina.length < 15) {
+    return true
+  }
+  return false
+});
+
+console.log(zucchinePiccole);
