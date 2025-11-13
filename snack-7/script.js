6cc17572949474f57e47fb70e1575f01ba1ba9d7
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-11-13 16:36:47 +0100

    feat: 7 done

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const secondoStudente = students.find(function (studente) {
+  return studente.id = 2;
+});
+
+console.log(secondoStudente);
